37b1402798f8dbc43f02439f09aa09b4c53b06af
CrazyGuy108 <taylor@hansenfamily.us>
2018-09-09 07:15:54 -0700

    Split logic into multiple files

---
+++ nn/main.py
@@ -0,0 +1,6 @@
+from generate import play_generated
+from train import train_adversarial, train_discriminator
+
+train_discriminator()
+train_adversarial()
+play_generated()
--- nn/mid.py
+++ nn/mid.py
@@ -56,6 +56,7 @@ note_to_int = dict((note, number) for number, note in enumerate(int_to_note))
 # number of different notes in dataset
 n_vocab = len(unique_notes)
 
+# real midi data
 real_d_in = []
 
 for i in range(0, len(songs)):
@@ -75,57 +76,3 @@ n_patterns = len(real_d_in)
 for i in range(len(real_d_in)):
     real_d_in[i] = np.array(real_d_in[i]).reshape(
         (1, len(real_d_in[i]), 3)) / float(n_vocab)
-
-# TODO: move to separate file
-
-from classical_nn import am, dm, gm, noise_length
-
-def get_noise_vector():
-    return np.random.uniform(size=(1, noise_length))
-
-# generate an amount of notes using the generator
-def generate(g_notes=500):
-    noise_vector = get_noise_vector()
-    g_in = np.full(shape=(1, g_notes, noise_length), fill_value=noise_vector)
-    return gm.predict(g_in, batch_size=1)
-
-# trains the discriminator network for one full epoch
-def train_discriminator():
-
-    # expected output for each type song
-    real = np.zeros(shape=(1, 1))
-    fake = np.ones(shape=(1, 1))
-
-    # generate training data
-    d_in = []
-    d_out = []
-    for i in range(len(real_d_in)):
-        d_in.append(real_d_in[i])
-        d_out.append(np.array(real))
-        d_in.append(generate(np.random.randint(100, 1000)).reshape((1, -1, 3)))
-        d_out.append(np.array(fake))
-
-    # train one full epoch with all the training data
-    for x, y in zip(d_in, d_out):
-        dm.train_on_batch(x, y)
-
-# trains the adversarial (generator+discriminator) network for one full batch
-def train_adversarial():
-    g_notes = np.random.randint(100, 1000)
-    noise_vector = get_noise_vector()
-    a_in = np.full(shape=(1, g_notes, noise_length), fill_value=noise_vector)
-    a_out = np.ones(shape=(1, 1))
-    am.train_on_batch(a_in, a_out)
-
-train_discriminator()
-train_adversarial()
-
-# test stuff
-generated_song = generate()[0]
-processed_song = []
-for note_data in generated_song:
-    processed_song.append([int_to_note[int(note_data[0] * n_vocab)],
-                note_data[1], note_data[2]])
-
-from create_midi import create_midi
-create_midi(processed_song)
